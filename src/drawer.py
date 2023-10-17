89bdd9c142da62183b5c9f8d2b68a98be72dc399
SatsukiHabami <mardiv.tby@gmail.com>
2023-10-17 16:41:49 +0900

    [add] entityの移動方法を変更
    entityの移動を自然にするために、dx,dyを指定するのではなくr,dθ を指定するように変更

--- src/drawer.py
+++ src/drawer.py
@@ -24,22 +24,29 @@ entity_types = {
     }
 }
 
+# entityの向いている方向(θ [rad]、右方向 0 rad)のリスト
+entities_directions: list[float] = []
 # [x,y] の組のリスト
 entities_coords: list[list] = []
 entities: list[plt.Circle] = []
 
 # 指定した座標にエンティティを配置する
-def add_entity(x:float, y:float, *, entitytype:str="normal"):
-    print("hello")
+def add_entity(x:float, y:float, *, direction:float=0, entitytype:str="normal"):
     entities_coords.append([x,y])
+    entities_directions.append(direction)
     entities.append(plt.Circle((x,y),
                                entity_types[entitytype]["radius"],
                                fc=entity_types[entitytype]["color"]))
     ax.add_patch(entities[-1])
 
+# ランダムな移動方向 dθ の元となる正規分布
+# 平均 0 rad, σ=0.7 radのリスト
+_rand_dirs = np.random.normal(0,0.7,100).tolist()
 def move_entity(idx:int):
-    dx = random.random()*4-2
-    dy = random.random()*4-2
+    entities_directions[idx] += _rand_dirs[random.randint(0,99)]
+    velocity = random.random()*3
+    dx = velocity * np.cos(entities_directions[idx])
+    dy = velocity * np.sin(entities_directions[idx])
     newc = [entities_coords[idx][0] + dx, entities_coords[idx][1] + dy]
     entities_coords[idx] = keep_coords_within_bounds(newc, entities_coords)
     entities[idx].center = tuple(entities_coords[idx])
@@ -48,7 +55,7 @@ def keep_coords_within_bounds(new_coords:list[float], old_coords:list[float]) ->
     if new_coords[0]>X_MAX:
         new_coords[0] = X_MAX
     elif new_coords[0]<X_MIN:
-        new_coords = X_MIN
+        new_coords[0] = X_MIN
 
     if new_coords[1]>Y_MAX:
         new_coords[1] = Y_MAX
@@ -65,11 +72,14 @@ def init():
         return []
     initialized = True
 
+    init_entities()
+    return entities
+
+def init_entities():
     for n in range(2):
         x = random.randrange(X_MIN,X_MAX,1)
         y = random.randrange(Y_MIN,Y_MAX,1)
         add_entity(x,y,entitytype="normal")
-    return entities
 
 # Animation function to update the frame
 def update(i):
@@ -78,7 +88,7 @@ def update(i):
     return entities
 
 # Create the animation
-anim = FuncAnimation(fig, update, init_func=init, frames=300, blit=True)
+anim = FuncAnimation(fig, update, init_func=init, frames=100, blit=True)
 
 # Display the animation
 plt.show()
