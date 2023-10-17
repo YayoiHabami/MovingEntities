e59ef9b92f2800956bb0910fddf7b64a523a6578
SatsukiHabami <mardiv.tby@gmail.com>
2023-10-17 18:44:19 +0900

    [update] matplotlibの隠蔽処理
    drawerクラスを他の部分から完全に分離した

--- src/drawer.py
+++ src/drawer.py
@@ -1,114 +1,45 @@
 from utils import (
-    X_MIN, X_MAX, Y_MIN, Y_MAX,
     get_random_coords
 )
 
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-from matplotlib.artist import Artist
-import numpy as np
-
+from abc import ABCMeta, abstractmethod
 import random
 
+class Drawer(metaclass=ABCMeta):
+    
+    # 領域
+    X_MIN = 0
+    X_MAX = 100
+    Y_MIN = 0
+    Y_MAX = 100
 
-# Create the figure and axis
-fig, ax = plt.subplots(figsize=(6,6))
-ax.set_xlim(X_MIN, X_MAX)
-ax.set_ylim(Y_MIN, Y_MAX)
-ax.set_xticks([])
-ax.set_yticks([])
+    @abstractmethod
+    def add_object(object:object) -> object:
+        pass
+    
+    @abstractmethod
+    def add_circle(x:float, y:float, *, radius:float=1, fc:str="black") -> object:
+        pass
 
+    @abstractmethod
+    def run(update, init=None):
+        """update: 更新関数, init:初期化関数"""
+        pass
 
+    def keep_coords_within_bounds(self, new_coords:list[float], old_coords:list[float]) -> list[float]:
+        if new_coords[0]>self.X_MAX:
+            new_coords[0] = self.X_MAX
+        elif new_coords[0]<self.X_MIN:
+            new_coords[0] = self.X_MIN
 
-# nutrition (栄養)
-nutritions: list[plt.Circle] = []
+        if new_coords[1]>self.Y_MAX:
+            new_coords[1] = self.Y_MAX
+        elif new_coords[1] < self.X_MIN:
+            new_coords[1] = self.X_MIN
 
-# entity
-# entityの向いている方向(θ [rad]、右方向 0 rad)のリスト
-entities_directions: list[float] = []
-# [x,y] の組のリスト
-entities_coords: list[list] = []
-entities: list[plt.Circle] = []
-entity_types = {
-    "normal": {
-        "radius": 1,
-        "color": "red"
-    }
-}
+        return new_coords
 
-# グラフ上にオブジェクトを追加する
-def add_object(object:Artist):
-    ax.add_patch(object)
+    def get_random_coords(self,xmin=X_MIN,xmax=X_MAX,ymin=Y_MIN,ymax=Y_MAX)-> tuple[float]:
+        """指定された領域の中でランダムな位置 (x,y) を返す"""
+        return random.randrange(xmin,xmax,1),random.randrange(ymin,ymax,1)
 
-# 描画用の円形オブジェクトを取得する
-def get_circle(x:float, y:float, *, radius:float=1, fc:str="black") -> plt.Circle:
-    return plt.Circle((x,y), radius, fc=fc)
-
-# 指定した座標にエンティティを配置する
-def add_entity(x:float, y:float, *, direction:float=0, entitytype:str="normal"):
-    entities_coords.append([x,y])
-    entities_directions.append(direction)
-    entities.append(get_circle(x,y,
-                               radius=entity_types[entitytype]["radius"],
-                               fc=entity_types[entitytype]["color"]))
-    add_object(entities[-1])
-
-# ランダムな移動方向 dθ の元となる正規分布
-# 平均 0 rad, σ=0.7 radのリスト
-_rand_dirs = np.random.normal(0,0.7,100).tolist()
-def move_entity(idx:int):
-    entities_directions[idx] += _rand_dirs[random.randint(0,99)]
-    velocity = random.random()*3
-    dx = velocity * np.cos(entities_directions[idx])
-    dy = velocity * np.sin(entities_directions[idx])
-    newc = [entities_coords[idx][0] + dx, entities_coords[idx][1] + dy]
-    entities_coords[idx] = keep_coords_within_bounds(newc, entities_coords)
-    entities[idx].center = tuple(entities_coords[idx])
-
-def keep_coords_within_bounds(new_coords:list[float], old_coords:list[float]) -> list[float]:
-    if new_coords[0]>X_MAX:
-        new_coords[0] = X_MAX
-    elif new_coords[0]<X_MIN:
-        new_coords[0] = X_MIN
-
-    if new_coords[1]>Y_MAX:
-        new_coords[1] = Y_MAX
-    elif new_coords[1] < X_MIN:
-        new_coords[1] = X_MIN
-
-    return new_coords
-
-# Initialize function to set up the initial frame
-initialized = False
-def init():
-    global initialized
-    if initialized:
-        return []
-    initialized = True
-
-    init_entities()
-    init_nutritions()
-    return entities + nutritions
-
-def init_entities():
-    for n in range(2):
-        x,y = get_random_coords()
-        add_entity(x,y,entitytype="normal")
-
-def init_nutritions():
-    for n in range(10):
-        x,y = get_random_coords()
-        nutritions.append(get_circle(x,y,radius=0.5,fc="green"))
-        add_object(nutritions[-1])
-        
-
-# Animation function to update the frame
-def update(i):
-    for n in range(len(entities)):
-        move_entity(n)
-
-# Create the animation
-anim = FuncAnimation(fig, update, init_func=init, frames=100)#, blit=True)
-
-# Display the animation
-plt.show()
---
+++ src/drawer_matplotlib.py
@@ -0,0 +1,33 @@
+
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from matplotlib.artist import Artist
+
+from drawer import (
+    Drawer
+)
+
+class PltDrawer(Drawer):
+
+    def __init__(self) -> None:
+        self.fig, self.ax = plt.subplots(figsize=(6,6))
+        self.ax.set_xlim(self.X_MIN, self.X_MAX)
+        self.ax.set_ylim(self.Y_MIN, self.Y_MAX)
+        self.ax.set_xticks([])
+        self.ax.set_yticks([])
+
+        super().__init__()
+    
+    def add_object(self, object:Artist) -> Artist:
+        self.ax.add_patch(object)
+        return object
+    
+    def add_circle(self, x: float, y: float, *, radius: float = 1, fc: str = "black") -> object:
+        return self.add_object(plt.Circle((x,y), radius, fc=fc))
+
+    def run(self, update, init=None):
+        # Create the animation
+        self.anim = FuncAnimation(self.fig, update, init_func=init, frames=100)#, blit=True)
+
+        # Display the animation
+        plt.show()
